13d66f2380ba01f65307f79443522ce77bdc1f6e
Ricky Han <rickylqhan@gmail.com>
2016-10-22 09:26:24 +0000

    finder dispatch

--- snacks.js
+++ snacks.js
@@ -1,37 +1,35 @@
-var code = `package com.hmkcode;
-
-
-import java.util.Map;
-import java.util.TreeMap;
-
-public class MapSort {
-
-  public static Map sortByValue(Map unsortedMap){
-  	Map sortedMap = new TreeMap(new ValueComparator(unsortedMap));
-  	sortedMap.putAll(unsortedMap);
-  	return sortedMap;
+var finderJava = function(code){
+  codeArray = code.split("\n");
+  var classMatch = /public class \w+/;
+  var methodMatch = /(public|private) (static)? \w+ \w+/;
+  var moc = [];
+  for(var i = 0; i < codeArray.length; i++)
+  {
+  	if(codeArray[i].match(classMatch)){
+  		var segment = codeArray[i].match(classMatch);
+  		var curClass = segment[0].replace(/public class /,"");
+  		moc.push({line: i, name: curClass, type: "class"})
+  	}
+  	if(codeArray[i].match(methodMatch)){
+  		var segment = codeArray[i].match(methodMatch);
+  		var curMethod = segment[0].replace(/(public|private) (static)? \w+ /,"");
+  		moc.push({line: i, name: curMethod, type: "method"})
+  	}
   }
-  public static Map sortByKey(Map unsortedMap){
-  	Map sortedMap = new TreeMap();
-  	sortedMap.putAll(unsortedMap);
-  	return sortedMap;
-  }
-}`;
-codeArray = code.split("\n");
-var classMatch = /public class \w+/;
-var methodMatch = /(public|private) (static)? \w+ \w+/;
-var moc = [];
-for(var i = 0; i < codeArray.length; i++)
-{
-	if(codeArray[i].match(classMatch)){
-		var segment = codeArray[i].match(classMatch);
-		var curClass = segment[0].replace(/public class /,"");
-		moc.push({line: i, name: curClass, type: "class"})
-	}
-	if(codeArray[i].match(methodMatch)){
-		var segment = codeArray[i].match(methodMatch);
-		var curMethod = segment[0].replace(/(public|private) (static)? \w+ /,"");
-		moc.push({line: i, name: curMethod, type: "method"})
-	}
+  return moc;
 }
-console.log(moc);
+
+var finderJavascript = function(code) {
+  return null;
+};
+
+var finder = function(extension){
+  return {
+    '.java': finderJava,
+    '.js', finderJavascript,
+  }[extension];
+}
+
+module.exports = {
+  finder
+}
